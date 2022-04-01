108c371c8d8a09dab9f4fe2e71bc85704b50d7c7
1Nathane <nathane.miranda@gmail.com>
2022-04-01 02:31:46 -0300

    aggregate

--- src/api/batch/batchService.js
+++ src/api/batch/batchService.js
@@ -1,9 +1,10 @@
 const Batch = require('./batch')
 const errorHandler = require('../common/errorHandler')
 const moment = require('moment')
+const Product = require('../product/product')
 
 Batch.methods(['get', 'post', 'put', 'delete'])
-Batch.updateOptions({new: true, runValidators: true})
+Batch.updateOptions({ new: true, runValidators: true })
 Batch.after('post', errorHandler).after('put', errorHandler)
 
 function daysPlusDate(data, dias) {
@@ -13,58 +14,89 @@ function daysPlusDate(data, dias) {
 
 Batch.route('count', (req, res, next) => {
     Batch.count((error, value) => {
-        if(error) {
-            res.status(500).json({errors: [error]})
+        if (error) {
+            res.status(500).json({ errors: [error] })
         } else {
-            res.json({value})
+            res.json({ value })
         }
     })
 })
 
 Batch.route('expired', (req, res, next) => {
-    Batch.count( {outputDate:{$lt: Date.now()}}         
-    , (error, result) => {
-        if(error) {
-            res.status(500).json({errors: [error]})
-        } else {         
-            res.json({expired: result})
-        }   
-    })
+    Batch.count({ outputDate: { $lt: Date.now() } }
+        , (error, result) => {
+            if (error) {
+                res.status(500).json({ errors: [error] })
+            } else {
+                res.json({ expired: result })
+            }
+        })
 })
 
 Batch.route('expiremonth', (req, res, next) => {
-    Batch.count({$and:[{outputDate:{$gt: Date.now()}},{outputDate:{$lt: daysPlusDate(Date.now(),30)}}]}, (error, result) => {
-        if(error) {
-            res.status(500).json({errors: [error]})
+    Batch.count({ $and: [{ outputDate: { $gt: Date.now() } }, { outputDate: { $lt: daysPlusDate(Date.now(), 30) } }] }, (error, result) => {
+        if (error) {
+            res.status(500).json({ errors: [error] })
         } else {
-            res.json({expiremonth: result})
-        }   
+            res.json({ expiremonth: result })
+        }
     })
 })
 
 Batch.route('expireaftermonth', (req, res, next) => {
-    Batch.count({outputDate:{$gt: daysPlusDate(Date.now(),30)}}, (error, result) => {
-        if(error) {
-            res.status(500).json({errors: [error]})
+    Batch.count({ outputDate: { $gt: daysPlusDate(Date.now(), 30) } }, (error, result) => {
+        if (error) {
+            res.status(500).json({ errors: [error] })
         } else {
-            res.json({expireaftermonth: result})
-        }   
+            res.json({ expireaftermonth: result })
+        }
     })
 })
 
 Batch.route('summary', (req, res, next) => {
+
     Batch.aggregate([
-        {$group : {
-            _id: null,
-            expired: {$count:{"$outputDate":{$gt: daysPlusDate(Date.now(),30)}}}
-        }}
-    , (error, result) => {
-        if(error) {
-            res.status(500).json({errors: [error]})
-        } else {
-            res.json({result})
-        }   
-    }])
+        {
+            $match:
+            {
+               status: 'ABERTO' 
+            }
+        },{
+           $lookup:
+           {
+               from: "products",
+               localField: "product_id",
+               foreignField:"_id",
+               as: "product"
+           }
+        },{
+            $project:
+            {
+                batch: "$name",
+                user_email: '$user_email',
+                product: '$product.name',
+                inputDate: '$inputDate',
+                outputDate:'$outputDate',
+                quantity: "$quantity"
+            }
+        },{
+            $sort:
+            {
+                outputDate: 1
+            }
+        }
+
+
+
+    ])
+
+        .exec((error, result) => {
+            if (error) {
+                res.status(500).json({ errors: [error] })
+            } else {
+                res.json( result )
+            }
+        })
 })
 
 
