e66f4be2b5252b8f7600032e21b9ee0198ed3e36
1Nathane <nathane.miranda@gmail.com>
2022-05-26 21:31:59 -0300

    a

--- src/api/brand/brandService.js
+++ src/api/brand/brandService.js
@@ -15,42 +15,43 @@ Brand.route('list', (req, res, next) => {
     })
 })
 
-Brand.route('count', (req, res, next) => {
-    Brand.count((error, value) => {
-        if(error) {
-            res.status(500).json({errors: [error]})
-        } else {
-            res.json({value})
-        }
-    })
-})
-
 // Brand.route('count', (req, res, next) => {
-
-//     Batch.aggregate([
-//         {
-//             $match:
-//             {
-//                 user_email: req.headers.email 
-//             }
-//         },{
-//             $project: {
-//                 _id: "$_id.user_email",
-//                total: { $sum: 1 }
-//             }
+//     Brand.count((error, value) => {
+//         if(error) {
+//             res.status(500).json({errors: [error]})
+//         } else {
+//             res.json({value})
 //         }
-
-
-
-//     ])
-
-//         .exec((error, result) => {
-//             if (error) {
-//                 res.status(500).json({ errors: [error] })
-//             } else {
-//                 res.json( result )
-//             }
-//         })
+//     })
 // })
+
+Brand.route('count', (req, res, next) => {
+
+    Batch.aggregate([
+        // {
+        //     $match:
+        //     {
+        //         user_email: req.headers.email 
+        //     }
+        // },
+        {
+            $project: {
+                _id: "$_id.user_email",
+               total: { $sum: 1 }
+            }
+        }
+
+
+
+    ])
+
+        .exec((error, result) => {
+            if (error) {
+                res.status(500).json({ errors: [error] })
+            } else {
+                res.json( result )
+            }
+        })
+})
 
 module.exports = Brand
